921b8a772a5ce2b83e4fbdea489d700babf2a00f
jehadadhame <22211052@students.hebron.edu>
2025-06-02 02:41:41 +0300

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ use App\Models\HealthCareProvider;
 use App\Models\Nutrient;
 use App\Models\Patient;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,28 +17,42 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    protected static ?string $password;
     public function run(): void
     {
-        Patient::factory()->create([
+
+        Patient::create([
             'name' => 'patient 0',
             'email' => 'patient_0@example.com',
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ]);
 
-        HealthCareProvider::factory()->create([
+        HealthCareProvider::create([
             'name' => 'doctor 0',
             'email' => 'doctor_0@example.com',
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
             'service_type' => "doctors"
         ]);
 
-        HealthCareProvider::factory()->create([
+        HealthCareProvider::create([
             'name' => 'dietitian 0',
             'email' => 'dietitian_0@example.com',
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
             'service_type' => "dietitians"
         ]);
 
-        Administrator::factory()->create([
+        Administrator::create([
             'name' => 'admin 0',
             'email' => 'admin_0@example.com',
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ]);
 
         $this->call(FoodCategorySeeder::class);
